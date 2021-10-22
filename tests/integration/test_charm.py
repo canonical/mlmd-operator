6a8cac7eac3f9a775b8219de373505faa9082908
Aggelos Kolaitis <neoaggelos@gmail.com>
2021-10-22 10:42:23 +0300

    Sort imports and minor logging fixes in integration tests

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -2,11 +2,12 @@ import logging
 import os
 import subprocess
 from pathlib import Path
-import pytest
 import yaml
+
+import pytest
 from pytest_operator.plugin import OpsTest
 
-log = logging.getLogger(__name__)
+LOG = logging.getLogger(__name__)
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 
@@ -14,7 +15,7 @@ METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test: OpsTest):
     built_charm_path = await ops_test.build_charm(".")
-    log.info(f"Built charm {built_charm_path}")
+    LOG.info(f"Built charm {built_charm_path}")
 
     image_path = METADATA["resources"]["oci-image"]["upstream-source"]
     resources = {"oci-image": image_path}
@@ -32,7 +33,7 @@ async def test_using_charm(ops_test: OpsTest, tmp_path: Path):
     """
     script_abs_path = Path("tests/integration/data/interact_with_mlmd.sh").absolute()
 
-    logging.info(f"Using temporary directory {tmp_path}")
-    logging.info(f"cwd = {os.getcwd()}")
-    logging.info(f"script_abs_path = {script_abs_path}")
+    LOG.info(f"Using temporary directory {tmp_path}")
+    LOG.info(f"cwd = {os.getcwd()}")
+    LOG.info(f"script_abs_path = {script_abs_path}")
     subprocess.run([script_abs_path, ops_test.model_name], cwd=tmp_path, check=True)
