193afd7410563efae5903a5b0fe438d4aeb3b23e
Aggelos Kolaitis <neoaggelos@gmail.com>
2021-10-22 10:44:42 +0300

    Re-write unit tests for pebble

--- tests/unit/test_operator.py
+++ tests/unit/test_operator.py
@@ -1,80 +1,110 @@
-import pytest
-from ops.model import ActiveStatus, BlockedStatus
+from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
 from ops.testing import Harness
 
 from charm import Operator
 
-
-@pytest.fixture
-def harness():
-    return Harness(Operator)
+import unittest
+from unittest.mock import patch, MagicMock
 
 
-def test_not_leader(harness):
-    harness.begin()
-    assert harness.charm.model.unit.status == ActiveStatus("")
+class TestCharm(unittest.TestCase):
+    def setUp(self):
+        self.harness = Harness(Operator)
+        self.addCleanup(self.harness.cleanup)
 
+    def test_not_leader(self):
+        self.harness.begin()
+        self.assertEqual(self.harness.charm.unit.status, ActiveStatus())
 
-def test_missing_image(harness):
-    harness.set_leader(True)
-    harness.begin_with_initial_hooks()
-    assert harness.charm.model.unit.status == BlockedStatus(
-        "Missing resource: oci-image"
-    )
+    @patch("charm.KubernetesServicePatch", MagicMock())
+    def test_no_relation(self):
+        self.harness.set_leader(True)
+        with patch("ops.model.Container.push") as _push:
+            self.harness.begin_with_initial_hooks()
+            _push.assert_called_with(
+                "/config/config.proto",
+                'connection_config: {sqlite: {filename_uri: "file:/data/mlmd.db"}}',
+                make_dirs=True,
+            )
+        self.assertEqual(self.harness.charm.unit.status, ActiveStatus())
 
+        service = self.harness.model.unit.get_container("mlmd").get_service("mlmd")
+        self.assertTrue(service.is_running())
 
-def test_no_relation(harness):
-    harness.set_leader(True)
-    harness.add_oci_resource(
-        "oci-image",
-        {
-            "registrypath": "ci-test",
-            "username": "",
-            "password": "",
-        },
-    )
-    harness.begin_with_initial_hooks()
+        plan = self.harness.get_container_pebble_plan("mlmd").to_dict()
+        self.assertEqual(
+            plan["services"]["mlmd"],
+            {
+                "override": "replace",
+                "summary": "mlmd",
+                "startup": "enabled",
+                "command": "/bin/metadata_store_server"
+                " --metadata_store_server_config_file=/config/config.proto"
+                " --grpc_port=8080"
+                " --enable_database_upgrade=true",
+            },
+        )
 
-    assert harness.charm.model.unit.status == ActiveStatus("")
+    @patch("charm.KubernetesServicePatch", MagicMock())
+    def test_many_relations(self):
+        self.harness.set_leader(True)
+        self.harness.add_relation("mysql", "mysql")
+        self.harness.add_relation("mysql", "mysql2")
+        self.harness.begin_with_initial_hooks()
 
+        self.assertEqual(
+            self.harness.charm.unit.status, BlockedStatus("Too many mysql relations")
+        )
 
-def test_one_relation(harness):
-    harness.set_leader(True)
-    harness.add_oci_resource(
-        "oci-image",
-        {
-            "registrypath": "ci-test",
-            "username": "",
-            "password": "",
-        },
-    )
-    rel_id = harness.add_relation("mysql", "mysql")
-    harness.add_relation_unit(rel_id, "mysql/0")
-    harness.update_relation_data(
-        rel_id,
-        "mysql/0",
-        {
-            "database": "unit-db",
-            "host": "unit-host",
-            "port": "unit-port",
-            "user": "unit-user",
-            "password": "unit-password",
-            "root_password": "unit-root",
-        },
-    )
+    @patch("charm.KubernetesServicePatch", MagicMock())
+    def test_one_relation(self):
+        self.harness.set_leader(True)
+        rel_id = self.harness.add_relation("mysql", "mysql")
+        self.harness.add_relation_unit(rel_id, "mysql/0")
+        self.harness.update_relation_data(
+            rel_id,
+            "mysql/0",
+            {
+                "database": "unit-db",
+                "host": "unit-host",
+                "port": "unit-port",
+                "user": "unit-user",
+                "password": "unit-password",
+                "root_password": "unit-root",
+            },
+        )
 
-    harness.begin_with_initial_hooks()
+        self.harness.begin_with_initial_hooks()
+        self.assertEqual(self.harness.charm.unit.status, ActiveStatus())
 
-    pod_spec, _ = harness.get_pod_spec()
+        service = self.harness.model.unit.get_container("mlmd").get_service("mlmd")
+        self.assertTrue(service.is_running())
 
-    assert pod_spec["containers"][0]["args"] == [
-        "--mysql_config_database=unit-db",
-        "--mysql_config_host=unit-host",
-        "--mysql_config_port=unit-port",
-        "--mysql_config_user=unit-user",
-        "--mysql_config_password=unit-password",
-        "--grpc_port=8080",
-        "--enable_database_upgrade=true",
-    ]
+        plan = self.harness.get_container_pebble_plan("mlmd").to_dict()
+        self.assertEqual(
+            plan["services"]["mlmd"],
+            {
+                "override": "replace",
+                "summary": "mlmd",
+                "startup": "enabled",
+                "command": "/bin/metadata_store_server"
+                " --mysql_config_database=unit-db"
+                " --mysql_config_host=unit-host"
+                " --mysql_config_port=unit-port"
+                " --mysql_config_user=unit-user"
+                " --mysql_config_password=unit-password"
+                " --grpc_port=8080"
+                " --enable_database_upgrade=true",
+            },
+        )
 
-    assert harness.charm.model.unit.status == ActiveStatus("")
+    @patch("charm.KubernetesServicePatch", MagicMock())
+    def test_pebble_not_ready(self):
+        self.harness.set_leader(True)
+        with patch("ops.model.Container.can_connect") as _can_connect:
+            _can_connect.return_value = False
+            self.harness.begin_with_initial_hooks()
+
+        self.assertEqual(
+            self.harness.charm.unit.status, WaitingStatus("Waiting for pebble")
+        )
