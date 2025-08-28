ac7c58e647ff82aa9ce481aaa9fccbc16fda9fe1
Yurii Kondrakov <yurii.kondrakov@canonical.com>
2025-08-28 09:42:39 -0400

    feat: velero backup config (#148)
    * feat: velero backup config
    
    * feat: wire the interface tests
    
    * chore: remove lib-check CI job
    
    * chore: update dependencies
    
    * fix: unit test
    
    * chore: update velero lib
    
    * fix: switch from requirer to provider
    
    * fix: mock KubernetesServicePatch for interface test
    
    * chore: add persistentvolumes to the list of resources
    
    * chore: update velero lib
    
    Signed-off-by: misohu <michal.hucko@canonical.com>

--- src/charm.py
+++ src/charm.py
@@ -15,6 +15,7 @@ from charmed_kubeflow_chisme.kubernetes import create_charm_default_labels
 from charms.loki_k8s.v1.loki_push_api import LogForwarder
 from charms.mlops_libs.v0.k8s_service_info import KubernetesServiceInfoProvider
 from charms.observability_libs.v1.kubernetes_service_patch import KubernetesServicePatch
+from charms.velero_libs.v0.velero_backup_config import VeleroBackupProvider, VeleroBackupSpec
 from lightkube.models.core_v1 import ServicePort
 from lightkube.resources.core_v1 import Service
 from ops import main
@@ -115,6 +116,20 @@ class Operator(CharmBase):
             port=self._svc_grpc_port,
             refresh_event=self.on.config_changed,
         )
+
+        # configure the Velero backup relation
+        self.velero_backup_config = VeleroBackupProvider(
+            charm=self,
+            relation_name="velero-backup-config",
+            spec=VeleroBackupSpec(
+                include_namespaces=[self.model.name],
+                include_resources=["persistentvolumeclaims", "persistentvolumes"],
+                label_selector={
+                    "app.kubernetes.io/name": self.app.name,
+                },
+            ),
+        )
+
         self._logging = LogForwarder(charm=self)
 
 
